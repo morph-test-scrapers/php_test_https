3ede6d4bc2cb137d58723ea60a5f495713559416
Henare Degan <henare.degan@gmail.com>
2016-02-10 17:07:50 +1100

    Test scraping the problematic URL

--- scraper.php
+++ scraper.php
@@ -2,12 +2,13 @@
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
+
+// Read in a page
+$html = scraperwiki::scrape("https://ecouncil.wollondilly.nsw.gov.au/eservice/dialog/daEnquiryInit.do?nodeNum=40801");
+var_dump($html);
+
 // // Find something on the page using css selectors
 // $dom = new simple_html_dom();
 // $dom->load($html);
